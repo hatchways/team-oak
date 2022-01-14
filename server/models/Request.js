fdfddedb447110ef14ccc81ddee5f11c9fcfad69
Chad Napper <contact@chadnaps.com>
2022-01-13 18:47:02 -0700

    Add request model

--- server/models/Request.js
+++ server/models/Request.js
@@ -1,7 +1,64 @@
 const mongoose = require("mongoose");
 
+const addressSchema = new mongoose.Schema({
+  pickup: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  service: {
+    type: String,
+    ref: "Profile",
+    required: true,
+    default: null,
+  },
+  dropoff: {
+    type: String,
+    default: function () {
+      if (this.pickup) {
+        return this.pickup;
+      }
+
+      return null;
+    },
+  },
+});
+
 const requestSchema = new mongoose.Schema({
-  // TODO
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  sitterId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  accepted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  declined: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  address: addressSchema,
 });
 
 module.exports = Request = mongoose.model("Request", requestSchema);
