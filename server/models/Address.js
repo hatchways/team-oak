01076aa70ec235055ef902b30b30a995b52deb8e
Chad Napper <contact@chadnaps.com>
2022-01-15 13:04:32 -0700

    Update address model
    Replace previous version with a more intuitive version.

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,24 +1,48 @@
 const mongoose = require("mongoose");
 
 exports.addressSchema = new mongoose.Schema({
-  pickup: {
+  houseNumber: {
+    type: String || Number,
+    required: true,
+    maxlength: 25,
+  },
+  street: {
     type: String,
     required: true,
-    default: null,
+    maxlength: 50,
   },
-  service: {
+  district: {
     type: String,
+    maxlength: 50,
+  },
+  city: {
+    type: String,
+    maxlength: 50,
     required: true,
-    default: null,
   },
-  dropoff: {
+  county: {
     type: String,
-    default: function () {
-      if (this.pickup) {
-        return this.pickup;
-      }
-
-      return null;
+    maxlength: 50,
+  },
+  postalCode: {
+    type: String,
+    maxlength: 7,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Allows: h2t-1b8; h2z 1b8; H2Z1B8
+        // Disallows: Z2T 1B8 (leading Z); H2T 1O3 (contains O); Leading Z,W or to contain D, F, I, O, Q or U
+        // Credit: https://stackoverflow.com/a/46761018
+        return /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i.test(
+          value
+        );
+      },
+      message: (props) => `${props.value} is not a valid postal code.`,
     },
+  },
+  country: {
+    type: String,
+    maxlength: 50,
+    required: true,
   },
 });
