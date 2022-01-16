41399b03580b319d2bf571c5c4091d31adb89ff6
Chad Napper <contact@chadnaps.com>
2022-01-16 13:46:51 -0700

    Add helperFunctions.js
    Contains functions that may prove useful in other parts of the app.

---
+++ server/utils/helperFunctions.js
@@ -0,0 +1,59 @@
+/*
+ * Contents:
+ * Public - validateQuery(query)
+ * Private - isObjectIdValid(id)
+ * Private - isArrayBoolean(values[])
+ */
+
+const ObjectId = require("mongoose").Types.ObjectId;
+
+function isObjectIdValid(id) {
+  if (ObjectId.isValid(id)) {
+    return String(new ObjectId(id)) === String(id);
+  } else {
+    return false;
+  }
+}
+
+module.exports.validateQuery = function validateQuery(query) {
+  if (!isObjectIdValid(query.userId)) {
+    res.status(400);
+    throw new Error("Invalid userId");
+  }
+
+  if (!isObjectIdValid(query.sitterId)) {
+    res.status(400);
+    throw new Error("Invalid sitterId");
+  }
+
+  if (new Date(query.start) > new Date(query.end)) {
+    res.status(400);
+    throw new Error("Start date cannot be after end date");
+  }
+
+  if (new Date(query.start) < new Date(Date.now())) {
+    res.status(400);
+    throw new Error("Start date cannot be in the past");
+  }
+
+  if (!isArrayBoolean(query.accepted, query.declined, query.paid)) {
+    res.status(400);
+    throw new Error("Must be boolean value");
+  }
+};
+
+function isArrayBoolean(...values) {
+  for (value of values) {
+    if (value === "true" || value === "1") {
+      value = true;
+    } else if (value === "false" || value === "0") {
+      value = false;
+    }
+
+    if (typeof value !== "boolean") {
+      throw new Error(`Value '${value}' must be boolean`);
+    }
+  }
+
+  return true;
+}
