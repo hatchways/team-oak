aeaacafaaa996ebc4daa068e3cad29eb0387d20e
Ahmad Habib <ahmadalkali69@gmail.com>
2022-01-18 10:48:04 +0100

    Embedded Period Schema into Schedule Schema

--- server/models/Availability.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schedule = require("./Schedule");
-
-const availabilitySchema = new mongoose.Schema({
-  schedules: [Schedule]
-});
-
-module.exports = Availability = mongoose.model("Availability", availabilitySchema);
--- server/models/Schedule.js
+++ server/models/Schedule.js
@@ -1,20 +1,86 @@
 
 const mongoose = require("mongoose");
-const Period = require("./Period");
 
 const scheduleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   active: {
     type: Boolean,
-    required: true
+    required: true,
   },
   periods: {
-    monday: [Period],
-    tuesday: [Period],
-    wednesday: [Period],
-    thursday: [Period],
-    friday: [Period],
-    satruday: [Period],
-    sunday: [Period],
+    monday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
+    tuesday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
+    wednesday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
+    thursday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
+    friday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
+    saturday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
+    sunday: {
+      startTime: {
+        type: Date,
+        required: true,
+      },
+      endTime: {
+        type: Date,
+        required: true,
+      },
+    },
   },
 });
 
