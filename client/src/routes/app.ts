a36ad638146afa3bde7c09a6cc1aa64de1ea8c33
Ahmad Habib <ahmadalkali69@gmail.com>
2022-01-18 10:22:27 +0100

    Merged main with branch to solve merge conflicts

---
+++ client/src/routes/app.ts
@@ -0,0 +1,46 @@
+import Login from '../pages/Login/Login';
+import Signup from '../pages/SignUp/SignUp';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import Settings from '../pages/Settings/Settings';
+import LandingPage from '../pages/LandingPage/LandingPage';
+import { AccountType } from '../types/AccountType';
+
+export interface AppRouteType {
+  to: string;
+  component: React.ComponentType;
+  canView: Array<string>;
+  exact: boolean;
+}
+
+export const appRoutes: AppRouteType[] = [
+  {
+    to: '/',
+    component: LandingPage,
+    canView: [AccountType.PET_OWNER, AccountType.PET_SITTER],
+    exact: true,
+  },
+  {
+    to: '/login',
+    component: Login,
+    canView: [AccountType.PET_OWNER, AccountType.PET_SITTER],
+    exact: true,
+  },
+  {
+    to: '/signup',
+    component: Signup,
+    canView: [AccountType.PET_OWNER, AccountType.PET_SITTER],
+    exact: true,
+  },
+  {
+    to: '/dashboard',
+    component: Dashboard,
+    canView: [AccountType.PET_OWNER, AccountType.PET_SITTER],
+    exact: true,
+  },
+  {
+    to: '/profile/settings',
+    component: Settings,
+    canView: [AccountType.PET_OWNER, AccountType.PET_SITTER],
+    exact: false,
+  },
+];
