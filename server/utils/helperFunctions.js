151fea49b6f8f9fe23c9011e1850424756dbad53
Chad Napper <contact@chadnaps.com>
2022-01-19 13:39:07 -0700

    Add more helperFunctions to utils/helperFunctions.js
    These functions are intended to be used in controller/request.js logic.

--- server/utils/helperFunctions.js
+++ server/utils/helperFunctions.js
@@ -1,11 +1,71 @@
 /*
- * Contents:
- * Public - validateQuery(query)
- * Private - isObjectIdValid(id)
- * Private - isArrayBoolean(values[])
+ * Contents
+ * Public:
+ *  - checkQueryForEmptyFields()
+ *  - updateRequest()
+ *  - validateQuery()
+ * Private:
+ *  - updateRequestField()
+ *  - isObjectIdValid()
+ *  - isArrayBoolean()
  */
 
 const ObjectId = require("mongoose").Types.ObjectId;
+
+/**
+ * Check req.query for specified empty fields
+ * @param {Object} req - Express request object
+ * @param {String} fieldsToCheck - Comma separated keys from req to check
+ * @returns {Array} Array of fields that were checked
+ */
+module.exports.checkQueryForEmptyFields = function checkQueryForEmptyFields(
+  req,
+  ...fieldsToCheck
+) {
+  const fieldsToChange = [];
+
+  for (const field of fieldsToCheck) {
+    if (typeof req.query[field] !== "undefined") {
+      fieldsToChange.push(field);
+    }
+  }
+
+  return fieldsToChange;
+};
+
+/**
+ * Update specified fields of Mongoose request object with values from req.query
+ * @param {Object} request - Mongoose request object
+ * @param {Array<String>} fieldsToChange - Array of fields to update
+ * @param {Object} req - Express request object
+ * @returns {Object} Updated Mongoose request object
+ */
+module.exports.updateRequest = function updateRequest(
+  request,
+  fieldsToChange,
+  req
+) {
+  for (const field of fieldsToChange) {
+    if (isArrayBoolean(req.query[field])) {
+      request.field = updateRequestField(request, field, req.query[field]);
+    } else {
+      throw new Error(`Field '${field}' is not boolean`);
+    }
+  }
+
+  return request;
+
+  function updateRequestField(requestObject, resource, newValue) {
+    if (
+      typeof newValue !== "undefined" &&
+      newValue !== requestObject.resource
+    ) {
+      return (requestObject[resource] = newValue);
+    }
+
+    return requestObject;
+  }
+};
 
 // Compares string version of id against string version of new ObjectId
 // with id provided as a parameter. If original id isn't a valid ObjectId,
