a68532ef9ca00b6f2d8861895290f754f7a98849
cl-codes <96796802+cl-codes@users.noreply.github.com>
2022-01-23 21:34:47 -0500

    used Formik

--- client/src/pages/Settings/Availability/Availability.tsx
+++ client/src/pages/Settings/Availability/Availability.tsx
@@ -5,9 +5,9 @@ import { Typography } from '@mui/material';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import GenerateFormInterface from './FormInterface';
+import { Formik, FormikHelpers, Field, validateYupSchema } from 'formik';
+import React, { useState } from 'react';
 
-const savedSchedules = ['Working hours', 'Holidays'];
-const currentSchedule = 'Working hours';
 const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 const generateSchedules = (scheduleName: string): JSX.Element => {
@@ -22,27 +22,97 @@ interface AvailabilityProps {
   header: string;
 }
 
+interface Values {
+  scheduleSelect: string;
+  MondayCheckbox: boolean;
+  TuesdayCheckbox: boolean;
+  WednesdayCheckbox: boolean;
+  ThursdayCheckbox: boolean;
+  FridayCheckbox: boolean;
+  SaturdayCheckbox: boolean;
+  SundayCheckbox: boolean;
+  MondayFrom: string;
+  TuesdayFrom: string;
+  WednesdayFrom: string;
+  ThursdayFrom: string;
+  FridayFrom: string;
+  SaturdayFrom: string;
+  SundayFrom: string;
+  MondayTo: string;
+  TuesdayTo: string;
+  WednesdayTo: string;
+  ThursdayTo: string;
+  FridayTo: string;
+  SaturdayTo: string;
+  SundayTo: string;
+}
+
 const Availability: React.FC<AvailabilityProps> = ({ header }) => {
+  const [schedules, setSchedules] = useState(['Working hours', 'Holidays']);
+
   return (
     <Box>
       <SettingHeader header={header} />
-      <Select
-        id="scheduleSelect"
-        sx={{ height: '35px', width: 'auto', marginRight: '1rem', fontWeight: 700 }}
-        name="currentSchedule"
-        defaultValue={currentSchedule}
+      <Formik
+        initialValues={{
+          scheduleSelect: schedules[0],
+          MondayCheckbox: true,
+          TuesdayCheckbox: true,
+          WednesdayCheckbox: true,
+          ThursdayCheckbox: true,
+          FridayCheckbox: true,
+          SaturdayCheckbox: true,
+          SundayCheckbox: true,
+          MondayFrom: '10',
+          TuesdayFrom: '10',
+          WednesdayFrom: '10',
+          ThursdayFrom: '10',
+          FridayFrom: '10',
+          SaturdayFrom: '10',
+          SundayFrom: '10',
+          MondayTo: '22',
+          TuesdayTo: '22',
+          WednesdayTo: '22',
+          ThursdayTo: '22',
+          FridayTo: '22',
+          SaturdayTo: '22',
+          SundayTo: '22',
+        }}
+        onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+          setTimeout(() => {
+            // alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
       >
-        {savedSchedules.map((schedule) => generateSchedules(schedule))}
-      </Select>
-      <Button variant="contained" color="primary" sx={{ height: '35px' }} disableElevation>
-        + New Schedule
-      </Button>
-      <Typography variant="h3" sx={{ fontWeight: 500, fontSize: '18px', marginTop: 4, marginBottom: 2 }}>
-        Set your weekly hours
-      </Typography>
-      <Box sx={{ border: '1px solid #dbdbdb', borderWidth: '1px 1px 0px 1px' }}>
-        {week.map((day) => GenerateFormInterface({ day }))}
-      </Box>
+        {({ values, setFieldValue, handleSubmit }) => (
+          <form>
+            <Field
+              id="scheduleSelect"
+              sx={{ height: '35px', width: 'auto', marginRight: '1rem', fontWeight: 700 }}
+              name="scheduleSelect"
+              as={Select}
+            >
+              {schedules.map((schedule) => generateSchedules(schedule))}
+            </Field>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ height: '35px' }}
+              disableElevation
+              onClick={() => setSchedules(schedules.concat(['New Schedule']))}
+            >
+              + New Schedule
+            </Button>
+            <Typography variant="h3" sx={{ fontWeight: 500, fontSize: '18px', marginTop: 4, marginBottom: 2 }}>
+              Set your weekly hours
+            </Typography>
+            <Box sx={{ border: '1px solid #dbdbdb', borderWidth: '1px 1px 0px 1px' }}>
+              {week.map((day) => GenerateFormInterface(day, values, setFieldValue, handleSubmit))}
+            </Box>
+          </form>
+        )}
+      </Formik>
     </Box>
   );
 };
--- client/src/pages/Settings/Availability/FormInterface.tsx
+++ client/src/pages/Settings/Availability/FormInterface.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import FormGroup from '@mui/material/FormGroup';
@@ -6,131 +5,149 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import { Field } from 'formik';
 
-interface GenerateFormInterface {
-  day: string;
-}
-
-const GenerateFormInterface: React.FC<GenerateFormInterface> = ({ day }) => {
-  const [from, setFrom] = useState('10');
-  const [to, setTo] = useState('22');
-  const [checked, setChecked] = useState(true);
-
-  const twentyFourHours = () => {
-    const output = [];
-    for (let i = 0; i < 24; i++) {
-      const listItem = <MenuItem value={i}>{`${i}:00`}</MenuItem>;
-      output.push(listItem);
-    }
-    return output;
-  };
-
-  return (
-    <Grid
-      container
-      columns={3}
-      sx={{
-        height: '4.5rem',
-        display: 'flex',
-        borderBottom: '1px solid #dbdbdb',
-        opacity: checked ? 1 : 0.4,
-      }}
-    >
-      <FormGroup sx={{ marginTop: 'auto', marginBottom: 'auto', width: '25%' }}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              id={`${day}Checkbox`}
-              checked={checked}
-              onClick={() => {
-                if (checked) {
-                  setFrom('0');
-                  setTo('0');
-                }
-                setChecked(!checked);
-              }}
-              sx={{ marginLeft: '1.5rem' }}
-            />
-          }
-          label={<Typography sx={{ fontSize: '15px', fontWeight: 500 }}>{day}</Typography>}
-          sx={{ fontSize: '25px', fontWeight: 700 }}
-        />
-      </FormGroup>
-
-      <FormControlLabel
-        labelPlacement="start"
-        control={
-          <Select
-            id={`${day}StartTime`}
-            name={`${day}StartTime`}
-            type="time"
-            value={from}
-            onChange={(e) => {
-              if (checked) {
-                setFrom(e.target.value);
-              }
-              if (checked && e.target.value == '23') {
-                setTo('0');
-              } else if (parseInt(e.target.value) >= parseInt(to)) {
-                setTo((parseInt(e.target.value) + 1).toString());
-              }
-            }}
-            sx={{ marginTop: 'auto', marginBottom: 'auto', width: '100px', height: '30px' }}
-            MenuProps={{
-              anchorOrigin: {
-                vertical: 'top',
-                horizontal: 'right',
-              },
-            }}
-          >
-            {twentyFourHours()}
-          </Select>
-        }
-        label={
-          <Typography sx={{ fontSize: '12px', fontWeight: 700, marginLeft: '1rem', marginRight: '0.5rem' }}>
-            FROM
-          </Typography>
-        }
-        sx={{ fontSize: '25px', fontWeight: 700, width: '35%' }}
-      />
-      <FormControlLabel
-        labelPlacement="start"
-        control={
-          <Select
-            id={`${day}EndTime`}
-            name={`${day}EndTime`}
-            type="time"
-            value={to}
-            onChange={(e) => {
-              if (checked) {
-                setTo(e.target.value);
-              }
-              if (checked && e.target.value == '0') {
-                setFrom('23');
-              } else if (parseInt(e.target.value) <= parseInt(from)) {
-                setFrom((parseInt(e.target.value) - 1).toString());
-              }
-            }}
-            sx={{ marginTop: 'auto', marginBottom: 'auto', width: '100px', height: '30px' }}
-            MenuProps={{
-              anchorOrigin: {
-                vertical: 'top',
-                horizontal: 'right',
-              },
-            }}
-          >
-            {twentyFourHours()}
-          </Select>
-        }
-        label={
-          <Typography sx={{ fontSize: '12px', fontWeight: 700, marginLeft: '1rem', marginRight: '0.5rem' }}>
-            TO
-          </Typography>
-        }
-        sx={{ fontSize: '25px', fontWeight: 700, width: '30%' }}
-      />
-    </Grid>
-  );
+const twentyFourHours = () => {
+  const output = [];
+  for (let i = 0; i < 24; i++) {
+    const listItem = <MenuItem value={i}>{`${i}:00`}</MenuItem>;
+    output.push(listItem);
+  }
+  return output;
 };
+
+const GenerateFormInterface = (day: string, values: any, setFieldValue: any, handleSubmit: any) => (
+  <Grid
+    container
+    columns={3}
+    sx={{
+      height: '4.5rem',
+      display: 'flex',
+      borderBottom: '1px solid #dbdbdb',
+      opacity: values[`${day}Checkbox`] ? 1 : 0.4,
+    }}
+  >
+    <FormGroup sx={{ marginTop: 'auto', marginBottom: 'auto', width: '25%' }}>
+      <FormControlLabel
+        control={
+          <Field
+            id={`${day}Checkbox`}
+            name={`${day}Checkbox`}
+            checked={values[`${day}Checkbox`]}
+            onChange={(e: any) => {
+              if (!e.target.checked) {
+                setFieldValue(`${day}From`, '0');
+                setFieldValue(`${day}To`, '0');
+              } else {
+                setFieldValue(`${day}From`, '10');
+                setFieldValue(`${day}To`, '22');
+              }
+              setFieldValue(`${day}Checkbox`, e.target.checked);
+              handleSubmit();
+            }}
+            sx={{ marginLeft: '1.5rem' }}
+            type="checkbox"
+            component={Checkbox}
+          />
+        }
+        label={<Typography sx={{ fontSize: '15px', fontWeight: 500 }}>{day}</Typography>}
+        sx={{ fontSize: '25px', fontWeight: 700 }}
+      />
+    </FormGroup>
+
+    <FormControlLabel
+      labelPlacement="start"
+      control={
+        <Field
+          id={`${day}From`}
+          name={`${day}From`}
+          type="time"
+          onChange={(e: any) => {
+            if (values[`${day}Checkbox`]) {
+              const val = e.target.value;
+              if (val == '23') {
+                setFieldValue(`${day}To`, '0');
+              } else if (val >= parseInt(values[`${day}To`])) {
+                setFieldValue(`${day}To`, (parseInt(val) + 1).toString());
+              }
+              setFieldValue(`${day}From`, val);
+              handleSubmit();
+            }
+          }}
+          sx={{ marginTop: 'auto', marginBottom: 'auto', width: '100px', height: '30px' }}
+          MenuProps={{
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'right',
+            },
+          }}
+          as={Select}
+        >
+          {twentyFourHours()}
+        </Field>
+      }
+      label={
+        <Typography
+          sx={{
+            fontSize: '12px',
+            fontWeight: 700,
+            marginLeft: '1rem',
+            marginRight: '0.5rem',
+            textTransform: 'uppercase',
+          }}
+        >
+          from
+        </Typography>
+      }
+      sx={{ fontSize: '25px', fontWeight: 700, width: '35%' }}
+    />
+    <FormControlLabel
+      labelPlacement="start"
+      control={
+        <Field
+          id={`${day}To`}
+          name={`${day}To`}
+          type="time"
+          onChange={(e: any) => {
+            if (values[`${day}Checkbox`]) {
+              const val = e.target.value;
+              if (val == '0') {
+                setFieldValue(`${day}From`, '23');
+              } else if (val <= parseInt(values[`${day}From`])) {
+                setFieldValue(`${day}From`, (parseInt(val) - 1).toString());
+              }
+              setFieldValue(`${day}To`, val);
+              handleSubmit();
+            }
+          }}
+          sx={{ marginTop: 'auto', marginBottom: 'auto', width: '100px', height: '30px' }}
+          MenuProps={{
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'right',
+            },
+          }}
+          as={Select}
+        >
+          {twentyFourHours()}
+        </Field>
+      }
+      label={
+        <Typography
+          sx={{
+            fontSize: '12px',
+            fontWeight: 700,
+            marginLeft: '1rem',
+            marginRight: '0.5rem',
+            textTransform: 'uppercase',
+          }}
+        >
+          to
+        </Typography>
+      }
+      sx={{ fontSize: '25px', fontWeight: 700, width: '30%' }}
+    />
+  </Grid>
+);
 
 export default GenerateFormInterface;
