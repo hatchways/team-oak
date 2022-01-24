db64558e277ca3fa5459ed98625d478297d66ed2
cl-codes <96796802+cl-codes@users.noreply.github.com>
2022-01-23 21:35:41 -0500

    removed commented-out lines

--- client/src/pages/Settings/Availability/Availability.tsx
+++ client/src/pages/Settings/Availability/Availability.tsx
@@ -80,7 +80,6 @@ const Availability: React.FC<AvailabilityProps> = ({ header }) => {
         }}
         onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
           setTimeout(() => {
-            // alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
           }, 500);
         }}
