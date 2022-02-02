8267312a3249e40a158ec2fe7e8abcfad799b83b
cl-codes <96796802+cl-codes@users.noreply.github.com>
2022-02-02 16:54:57 -0500

    Front-end draft

--- client/src/routes/app.ts
+++ client/src/routes/app.ts
@@ -4,6 +4,7 @@ import Dashboard from '../pages/Dashboard/Dashboard';
 import Settings from '../pages/Settings/Settings';
 import LandingPage from '../pages/LandingPage/LandingPage';
 import { AccountType } from '../types/AccountType';
+import CustomerBookings from '../pages/Customer Bookings/CustomerBookings';
 
 export interface AppRouteType {
   to: string;
@@ -41,6 +42,12 @@ export const appRoutes: AppRouteType[] = [
     to: '/profile/settings',
     component: Settings,
     canView: [AccountType.PET_OWNER, AccountType.PET_SITTER],
+    exact: false,
+  },
+  {
+    to: '/mysitters',
+    component: CustomerBookings,
+    canView: [AccountType.PET_OWNER],
     exact: false,
   },
 ];
